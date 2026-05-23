49d26086e27c01ecfc3f1a71d56e7a056b897ff0
Oscar Garcia Chz <oscarale46@gmail.com>
2026-05-23 02:45:24 -0600

    refactor: remove ts-expect-error directive from FeaturedProducts component

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,6 @@ export default async function Home({ searchParams }: HomeProps) {
       <CategoriesGrid />
 
       {/* Featured Collection Section */}
-      {/* @ts-expect-error Server Component */}
       <FeaturedProducts />
 
       {/* Brand Narrative Section */}
